afcce42c656153c4b09cd670224cda0f72b25d01
dimah <dimah@DEZ>
2016-06-01 21:16:59 +0300

    Добавьте файлы проекта.

---
+++ K13-1-Golovko-MainProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K13_1_Golovko_MainProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
